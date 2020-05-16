1500acb55f1391e88364806c3db1efb3aaef67c8
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-05-16 09:01:54 +0530

    Added all routes for Entry level

--- Entry Level/server/app.js
+++ Entry Level/server/app.js
@@ -6,7 +6,30 @@ const { apiPrefix } = require('./constants');
 
 const port = process.env.PORT || 3000;
 
+// GET
 app.get(`${apiPrefix}tasks`, (req, res) => {
+  res.send('Hello World');
+});
+
+app.get(`${apiPrefix}tasks/:taskId`, (req, res) => {
+  const { taskId } = req.params;
+  res.send('Post with id' + taskId);
+});
+
+// POST
+app.post(`${apiPrefix}tasks`, (req, res) => {
+  res.send('Hello World');
+});
+
+// PUT
+app.put(`${apiPrefix}tasks/:taskId`, (req, res) => {
+  const { taskId } = req.params;
+  res.send('Hello World');
+});
+
+// DELETE
+app.delete(`${apiPrefix}tasks/:taskId`, (req, res) => {
+  const { taskId } = req.params;
   res.send('Hello World');
 });
 
