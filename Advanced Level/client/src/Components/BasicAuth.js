d43d1375871bdad36709d10d921ee4979d5241af
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-05-26 23:12:07 +0530

    basic auth signup made like sign in

--- Advanced Level/client/src/Components/BasicAuth.js
+++ Advanced Level/client/src/Components/BasicAuth.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function BasicAuth() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log('event:', event, username, password);
+    fetch('/api/v1/basic-auth', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div>
-      <form method="POST" action="/api/v1/basic-auth">
+      {/* <form method="POST" action="/api/v1/basic-auth" onSubmit={onSubmit}> */}
+      <form onSubmit={onSubmit} method="POST">
         <div>
           <label htmlFor="username">Username</label>
-          <input type="username" name="username" id="username"></input>
+          <input
+            type="username"
+            name="username"
+            id="username"
+            onChange={handleUsername}
+          ></input>
         </div>
         <div>
           <label htmlFor="password">password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handlePassword}
+          />
         </div>
         <div>
           <input type="submit" value="Submit"></input>
--- Advanced Level/server/app.js
+++ Advanced Level/server/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// password authentication
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 // token confirmation
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
@@ -42,7 +46,34 @@ app.get('/google-oauth', (req, res) => {
 
 app.post('/api/v1/basic-auth', (req, res) => {
   console.log('req.body:', req.body);
-  res.send('success');
+  const { username, password } = req.body;
+
+  // encrypt password
+  bcrypt.hash(password, 10, (err, encryptedPassword) => {
+    console.log('encryptedPassword:', encryptedPassword);
+
+    // dummy check
+    // bcrypt
+    //   .compare(password, encryptedPassword)
+    //   .then((res) => console.log('isPasswordSame:', res));
+
+    // Save in db
+  });
+
+  // create token
+  console.log(
+    'username, process.env.JWT_PRIVATE_KEY:',
+    username,
+    process.env.JWT_PRIVATE_KEY
+  );
+  const token = jwt.sign({ username }, process.env.JWT_PRIVATE_KEY);
+  console.log('token:', token);
+
+  // dummy check
+  // const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+  // console.log('decoded:', decoded);
+
+  res.send({ token });
 });
 
 app.listen(port, () => console.log('Server is running in port:', port));
