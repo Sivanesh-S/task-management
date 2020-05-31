610fbac20abf524484c40ae81779dfacf9279b7c
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-01 02:01:08 +0530

    Added objectId validating function

--- Advanced Level/server/utils/index.js
+++ Advanced Level/server/utils/index.js
@@ -1,3 +1,5 @@
+const { ObjectID } = require('mongodb');
+
 const isUpdated = (response) => {
   if (response.lastErrorObject.n) {
     return true;
@@ -5,6 +7,17 @@ const isUpdated = (response) => {
   return false;
 };
 
+const isValidId = (id) => {
+  try {
+    let objectId = new ObjectID(id);
+    return true;
+  } catch (error) {
+    console.log('Invalid object id error:', error.message);
+    throw new Error(error.message);
+  }
+};
+
 module.exports = {
   isUpdated,
+  isValidId,
 };
