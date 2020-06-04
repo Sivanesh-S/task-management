97248e1ff37cde4ad4b10cb3062727bbb07d60d4
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-04 19:56:22 +0530

    Made the card size small

--- Advanced Level/client/src/Components/TaskItem/TaskItem.js
+++ Advanced Level/client/src/Components/TaskItem/TaskItem.js
@@ -17,7 +17,7 @@ const onChange = () => {};
 function TaskItem() {
   return (
     <div>
-      <Card className={style.task} hoverable>
+      <Card className={style.task} hoverable size="small">
         <div className={style.container}>
           <Checkbox onChange={onChange}>
             aa asdasdwdknew fhgwbd bw dwh4e d 4ewdh we asdasd asda da sd
@@ -27,7 +27,7 @@ function TaskItem() {
           </a>
         </div>
       </Card>
-      <Card className={style.task} hoverable>
+      <Card className={style.task} hoverable size="small">
         <div className={style.container}>
           <Checkbox onChange={onChange}>aa asdasdq asd</Checkbox>
           <a href="#" className={style.option}>
