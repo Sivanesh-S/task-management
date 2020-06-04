630f8e32d852e710b85c53746c6177042a106f24
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-05 00:05:37 +0530

    password validation

--- Advanced Level/client/src/utils/index.js
+++ Advanced Level/client/src/utils/index.js
@@ -2,3 +2,20 @@ export function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+export function isInvalidPassword(password) {
+  if (!password) {
+    return 'Please enter the password';
+  }
+
+  if (password.length < 8) {
+    return 'Your password must be at least 8 characters';
+  }
+  if (password.search(/[a-z]/i) < 0) {
+    return 'Your password must contain at least one letter.';
+  }
+  if (password.search(/[0-9]/) < 0) {
+    return 'Your password must contain at least one digit.';
+  }
+  return false;
+}
