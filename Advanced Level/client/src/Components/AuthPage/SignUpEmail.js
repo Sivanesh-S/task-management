700342943e20939b6a2aef0b49d08ca5584078c4
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-05 01:31:47 +0530

    handled signup

--- Advanced Level/client/src/Components/AuthPage/SignUpEmail.js
+++ Advanced Level/client/src/Components/AuthPage/SignUpEmail.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// hooks
+import { useInput, usePrevious } from '../../hooks';
 
 // routing
 import { useHistory } from 'react-router-dom';
@@ -10,15 +13,96 @@ import style from './AuthPage.module.css';
 // icons
 import { FaArrowLeft, FaUser, FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 
+// utils
+import { validateEmail, isInvalidPassword } from '../../utils';
+
 // components
-import { Typography, Input } from 'antd';
+import { Typography, Input, message } from 'antd';
 const { Title } = Typography;
 
 function SignUpEmail(props) {
+  const [email, setEmail] = useInput('');
+  const [fullName, setFullName] = useInput('');
+  const [password, setPassword] = useInput('');
+  const [confirmPassword, setConfirmPassword] = useInput('');
+
+  // state
+  const [msg, setMsg] = useState('');
+  const prevMsg = usePrevious(msg);
+
   const history = useHistory();
 
   // routing
   const goBack = () => history.goBack();
+
+  useEffect(() => {
+    if (msg && prevMsg === msg) {
+      setMsg('');
+    }
+  }, [email, password]);
+
+  // events
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    if (
+      isEmailValid() &&
+      isPasswordValid() &&
+      isFullNameValid() &&
+      isPasswordConfirmed()
+    ) {
+      const response = await axios.post('/auth/signup', {
+        fullName,
+        email,
+        password,
+      });
+      console.log('response:', response);
+      const { token } = response.data;
+
+      if (token) {
+        localStorage.setItem('authKey', token);
+        history.push('/');
+        message.success(`Welcome To Twelve Tasks ${fullName}`);
+      } else {
+        setMsg("There's some issue in signup");
+      }
+    }
+  };
+
+  const isEmailValid = () => {
+    if (!validateEmail(email)) {
+      setMsg('Enter a valid Email');
+      return false;
+    }
+    return true;
+  };
+
+  const isPasswordValid = () => {
+    let errorString = null;
+    errorString = isInvalidPassword(password);
+    if (errorString) {
+      setMsg(errorString);
+      return false;
+    }
+    return true;
+  };
+
+  const isFullNameValid = () => {
+    if (fullName) {
+      return true;
+    }
+    setMsg('Full name must not be empty');
+    return false;
+  };
+
+  const isPasswordConfirmed = () => {
+    if (password === confirmPassword) {
+      return true;
+    }
+    setMsg(`Password and confirm password are not be same`);
+    return false;
+  };
+
   return (
     <div className={style.page}>
       <FaArrowLeft className={style.back} onClick={goBack} />
@@ -26,25 +110,45 @@ function SignUpEmail(props) {
       <Title>Twelve Tasks</Title>
       <Title level={3}>Sign up</Title>
       <div className={style.container}>
-        <Input placeholder="Email" type="email" className={style.input}></Input>
-        <Input
-          placeholder="Full Name"
-          type="text"
-          className={style.input}
-        ></Input>
-        <Input.Password
-          className={style.input}
-          placeholder="Password"
-          iconRender={(visible) => (visible ? <FaRegEye /> : <FaRegEyeSlash />)}
-        />
-        <Input.Password
-          className={style.input}
-          placeholder="Confirm Password"
-          visibilityToggle={false}
-        />
-        <button className={`${style.input} ${style.signUpButton}`} block>
-          Sign In
-        </button>
+        <form method="POST" onSubmit={onSubmit}>
+          <Input
+            value={email}
+            onChange={setEmail}
+            placeholder="Email"
+            type="text"
+            className={style.input}
+            onBlur={isEmailValid}
+          ></Input>
+          <Input
+            value={fullName}
+            onChange={setFullName}
+            placeholder="Full Name"
+            type="text"
+            className={style.input}
+            onBlur={isFullNameValid}
+          ></Input>
+          <Input.Password
+            value={password}
+            onChange={setPassword}
+            className={style.input}
+            placeholder="Password"
+            onBlur={isPasswordValid}
+            iconRender={(visible) =>
+              visible ? <FaRegEye /> : <FaRegEyeSlash />
+            }
+          />
+          <Input.Password
+            value={confirmPassword}
+            onChange={setConfirmPassword}
+            className={style.input}
+            placeholder="Confirm Password"
+            visibilityToggle={false}
+          />
+          {msg && <div style={{ color: 'crimson' }}>{msg}</div>}
+          <button className={`${style.input} ${style.signUpButton}`} block>
+            Sign In
+          </button>
+        </form>
         <div className={style.signUp}>
           Want to Join? <span className={style.signUpLink}>Sign Up</span>
         </div>
