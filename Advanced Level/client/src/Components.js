2d4c8586125bdf032cc847cb40bfd5dcc65d87e3
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-04 19:42:41 +0530

    Dummy component checker

---
+++ Advanced Level/client/src/Components.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+// Components
+import Login from './Components/Login';
+import SignUp from './Components/SignUp.js';
+import Header from './Components/Header/Header';
+import Analytics from './Components/Analytics/Analytics';
+import ViewSelect from './Components/ViewSelect/ViewSelect';
+import TaskItem from './Components/TaskItem/TaskItem';
+import RightMenu from './Components/RightMenu/RightMenu';
+import BottomTabs from './Components/BottomTabs/BottomTabs';
+import FilterPage from './Components/FilterPage/FilterPage';
+import AuthPage from './Components/AuthPage/AuthPage';
+import LoginEmail from './Components/AuthPage/LoginEmail';
+import SignUpEmail from './Components/AuthPage/SignUpEmail';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header />
+      <Analytics />
+      <ViewSelect />*/}
+      {/* <TaskItem /> */}
+      {/* <RightMenu /> */}
+      {/* <BottomTabs /> */}
+      {/* <FilterPage /> */}
+      {/* <AuthPage /> */}
+      <SignUpEmail />
+    </div>
+  );
+}
+
+export default App;
