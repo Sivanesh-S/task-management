e4a2e11f5634cd7ea3cc4e59264caa3e63f615a6
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-07 02:13:37 +0530

    added reducer for GET tasks, users

--- Advanced Level/client/src/context/reducer.js
+++ Advanced Level/client/src/context/reducer.js
@@ -1,8 +1,28 @@
+import { taskNormalizer } from './reducerUtils';
+
 export const reducerFunction = (state, action) => {
-  const { type } = action;
+  const { type, data } = action;
   switch (type) {
+    case 'GET_USER':
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...data,
+        },
+      };
+
+    case 'GET_TASKS':
+      return {
+        ...state,
+        tasks: {
+          ...state.tasks,
+          ...taskNormalizer(data),
+        },
+      };
+
     default:
-      throw new Error();
+      throw new Error('Type is not in reducer function. Go to reducer.js');
   }
 };
 
