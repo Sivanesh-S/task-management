c43ffd76971adc88a62d61361efe4478a2725a10
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2020-06-07 10:28:13 +0530

    handled analytics

--- Advanced Level/client/src/Components/Analytics/Analytics.js
+++ Advanced Level/client/src/Components/Analytics/Analytics.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import selectn from 'selectn';
 
 import style from './Analytics.module.css';
 
 // components
 import { Progress, Row, Col } from 'antd';
 
+// context
+import { store } from '../../context';
+
 function Analytics() {
+  const { state } = useContext(store);
+
+  const tasks = selectn('tasks', state);
+  const archived = selectn('archived', state);
+
+  if (!tasks || !archived) {
+    return null;
+  }
+
+  const getObjLength = (obj) => Object.keys(obj).length;
+
+  const tasksCount = getObjLength(tasks);
+  const archivedCount = getObjLength(archived);
+
+  const totalCount = tasksCount + archivedCount;
+
   return (
     <div className={style.analytics}>
       <Row>
         <Col span={12} style={{ textAlign: 'right', paddingRight: '15px' }}>
           <span>Task Completed: </span>
-          <span>13/43</span>
+          <span>{`${tasksCount}/${totalCount}`}</span>
         </Col>
         <Col span={12}>
           <Progress
-            percent={30}
-            successPercent={25}
-            status="active"
+            percent={(tasksCount / totalCount) * 100}
+            // successPercent={25} // if possible check and getList of in progress and add them
+            // status="active"
             showInfo={false}
           />
         </Col>
